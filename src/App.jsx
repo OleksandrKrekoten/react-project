568d95a6755dbe487a74827927b024eaae6b9c4b
Oleksandr <alexkrekoten11@gmail.com>
2023-04-06 17:45:48 +0300

    hero and header

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="reletive z-0 bg-sky-300 aspect-square">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="reletive z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
